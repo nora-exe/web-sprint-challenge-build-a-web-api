457d9e8289c940d7276eb742db3190f6ceabe3b1
Nora C <nocorser@gmail.com>
2021-07-09 16:37:39 -0500

    process.env.PORT || 5000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // START SERVER HERE
 const server = require('./api/server')
-const port = 5000
+const port = process.env.PORT || 5000
 
 // watch for connections on port 5000
 server.listen(port, () =>
